975f8da92ed33f81d8b0ef20d5f387b1f0e73819
Сергей <pent2006@gmail.com>
2023-10-28 21:19:04 +0300

    add class and methods for tests pages

--- tests/base_test.py
+++ tests/base_test.py
@@ -2,6 +2,7 @@ import pytest
 from pages.main_page import MainPage
 from pages.register_page import RegisterPage
 from pages.admin_login_page import AdminLoginPage
+from models.randoms.fake import random_letters, random_email
 
 class BaseTest:
     @pytest.fixture(autouse=True)
@@ -19,3 +20,24 @@ class BaseTest:
     @property
     def admin_login_page(self):
         return AdminLoginPage(self.browser)
+
+
+class BaseTestRegisterPage(BaseTest):
+    @pytest.fixture()
+    def create_new_user(self):
+        self.register_page.open_page(RegisterPage.UNIQUE_LOCATOR)
+        self.register_page.create_user(random_letters(),
+                                       random_email())
+
+
+class BaseTestAdminPage(BaseTest):
+    @pytest.fixture()
+    def login_admin_page(self):
+        self.admin_login_page.open()
+        self.admin_login_page.login("user",
+                                    "bitnami")
+        self.admin_login_page.click_modal_security_button()
+
+    @pytest.fixture()
+    def logout_admin_page(self, login_admin_page):
+        self.admin_login_page.click_logout_button()
