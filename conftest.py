7ca7ac8c66b6fcbb7ef5e0e33b5932c1fc4af08d
Сергей <pent2006@gmail.com>
2023-11-12 20:28:58 +0300

    add hook and fixture to add attachments to allure

--- conftest.py
+++ conftest.py
@@ -8,6 +8,14 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.firefox.options import Options as FFOptions
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 
+""" Information about tests results """
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    rep = outcome.get_result()
+
+    setattr(item, "rep_" + rep.when, rep)
+
 
 def pytest_addoption(parser):
     parser.addoption(
