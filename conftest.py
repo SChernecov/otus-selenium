393ade0cf0ead399f76b5dd4de788299e38e4cc9
Сергей <pent2006@gmail.com>
2023-11-18 23:00:54 +0300

    add remote

--- conftest.py
+++ conftest.py
@@ -1,5 +1,7 @@
+import os
 import datetime
 
+import allure
 import pytest
 import logging
 from selenium import webdriver
@@ -9,6 +11,8 @@ from selenium.webdriver.firefox.options import Options as FFOptions
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 
 """ Information about tests results """
+
+
 @pytest.hookimpl(tryfirst=True, hookwrapper=True)
 def pytest_runtest_makereport(item, call):
     outcome = yield
@@ -21,6 +25,11 @@ def pytest_addoption(parser):
     parser.addoption(
         "--browser", default="chrome",
         help="Browsers: chrome, firefox, opera or yandex"
+    )
+    parser.addoption(
+        "--chrome_driver_path",
+        default="C:\Drivers\chromedriver_win32\chromedriver.exe",
+        help="Path to chrome driver"
     )
     parser.addoption(
         "--yandex_driver_path",
@@ -53,6 +62,29 @@ def pytest_addoption(parser):
         default="DEBUG",
         help="Logging level: DEBUG, INFO, WARNING, ERROR or CRITICAL"
     )
+    parser.addoption(
+        "--remote",
+        help="Url to remote machine with selenium"
+    )
+    parser.addoption(
+        "--vnc",
+        action="store_true",
+        help="VNC option"
+    )
+    parser.addoption(
+        "--bv",
+        help="Browser version"
+    )
+    parser.addoption(
+        "--video",
+        action="store_true",
+        help="Video recording"
+    )
+    parser.addoption(
+        "--logs",
+        action="store_true",
+        help="Logs"
+    )
 
 
 @pytest.fixture()
@@ -60,6 +92,13 @@ def browser(request):
     headless = request.config.getoption("--headless")
     browser_name = request.config.getoption("--browser")
     log_level = request.config.getoption("--log_level")
+    remote = request.config.getoption("--remote")
+    vnc = request.config.getoption("--vnc")
+    version = request.config.getoption("--bv")
+    video = request.config.getoption("--video")
+    logs = request.config.getoption("--logs")
+
+    remote_url = f"http://{remote}:4444/wd/hub"
 
     logger = logging.getLogger(request.node.name)
     file_handler = logging.FileHandler(f"logs/{request.node.name}.log",
@@ -74,9 +113,14 @@ def browser(request):
 
     if browser_name == "chrome":
         options = ChromeOptions()
+        service = Service(
+            executable_path=request.config.getoption("--chrome_driver_path"))
         if headless:
             options.add_argument("--headless=new")
-        driver = webdriver.Chrome(options=options)
+        if remote:
+            driver = webdriver.Remote(remote_url, options=options)
+        else:
+            driver = webdriver.Chrome(service=service, options=options)
     elif browser_name == "firefox":
         options = FFOptions()
         if headless:
@@ -105,8 +149,31 @@ def browser(request):
         raise ValueError(
             f"Browser {browser_name} not supported."
             f" Chose between chrome, firefox, opera or yandex")
+
     if request.config.getoption("--max"):
         driver.maximize_window()
+
+    if request.config.getoption("--remote"):
+        caps = {
+            "browserName": browser_name,
+            "browserVersion": version,
+            "selenoid:options": {
+                "enableVNC": vnc,
+                "screenResolution": "1280x2000",
+                "enableVideo": video,
+                "enableLog": logs,
+                "timeZone": "Europe/Moscow"
+            },
+            "acceptInsecureCerts": True
+        }
+
+        for k, v in caps.items():
+            options.set_capability(k, v)
+
+        driver = webdriver.Remote(
+            command_executor=remote_url,
+            options=options
+        )
 
     driver.url = request.config.getoption("--url")
 
